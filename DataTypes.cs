4a4dc14fedeb966a9a336238d9f38fcfb8139f73
Boris Yakimov <burizz@users.noreply.github.com>
2016-10-26 16:12:50 +0200

    Update DataTypes.cs

--- DataTypes.cs
+++ DataTypes.cs
@@ -214,13 +214,13 @@ namespace LearningCSharp
             }
         }
 
-		public static void TakeDecimalDisplayBinary()
-		{
-			// Take decimal number and print its equivalent in binary
-			Console.WriteLine("Enter decimal number to be converted to binary");
-			int inputNumber = Convert.ToInt32(Console.ReadLine());
-			string numInBinary = Convert.ToString(inputNumber, 2);
-			Console.WriteLine("[{0}] converted to binary is [{1}]", inputNumber, numInBinary);
-		}
+	public static void TakeDecimalDisplayBinary()
+	{
+	    // Take decimal number and print its equivalent in binary
+	    Console.WriteLine("Enter decimal number to be converted to binary");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+	    string numInBinary = Convert.ToString(inputNumber, 2);
+	    Console.WriteLine("[{0}] converted to binary is [{1}]", inputNumber, numInBinary);
+	}
     }
 }
