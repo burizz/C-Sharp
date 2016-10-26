88a015f02e6ec39da35ab254a3cca44ed61bf54b
Boris Yakimov <burizz@users.noreply.github.com>
2016-10-26 10:35:16 +0200

    Update BasicExercises.cs

--- BasicExercises.cs
+++ BasicExercises.cs
@@ -5,6 +5,54 @@ namespace LearningCSharp
 {
     public class BasicExercise
     {
+        public static void Main()
+        {
+            // Add two numbers
+            int addOne = 8;
+            int addTwo = 2;
+            Console.WriteLine(Add(addOne, addTwo));
+
+            // Devide two numbers
+            Console.WriteLine(Devide(addOne, addTwo));
+
+            // Reversed numbers
+            int[] initialTwoNumbers = new int[] { 5, 6 };
+            int[] reversedNumbers = SwapNumbers(initialTwoNumbers);
+
+            Console.WriteLine("First Number : " + reversedNumbers[0]);
+            Console.WriteLine("Second Number : " + reversedNumbers[1]);
+
+            // Multiply three numbers
+            int[] intsToMultiply = new int[] { 2, 3, 6 };
+            Console.WriteLine(MultiplyThreeNums(intsToMultiply));
+
+            // Another implementation of the same MultiplyThreeNums method
+            MultiplyThreeNums();
+
+            // PrintExprOfTwoNumbers())
+            Console.Write("Enter a number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            PrintExprOfTwoNumbers(firstNumber, secondNumber);
+
+            // Print Multiplication table of a number - MultiplicationTable()
+            Console.WriteLine("Enter number bellow to see it's multiplication table");
+            Console.WriteLine("Number: ");
+            int multiplicationTableofNum = Convert.ToInt32(Console.ReadLine());
+
+            MultiplicationTable(multiplicationTableofNum);
+
+            // CalculateAverage(int[])
+            int[] arrayOfNumsForAverage = new int[] { 10, 15, 20, 30 };
+            CalculateAverage(arrayOfNumsForAverage);
+
+            // Convert Celsius to Fahrenheit and Kelvin
+            ConvertCeliusToKelvOrFahr(-30);
+        }
+        
         public static int Add(int addOne, int addTwo)
         // Add two numbers
         {
