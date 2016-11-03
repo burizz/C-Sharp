71365da6f5dfcb598171768d177ed71d214a5066
Boris Yakimov <burizz@users.noreply.github.com>
2016-11-03 11:51:46 +0100

    Update MissingNumbers.cs

--- MissingNumbers.cs
+++ MissingNumbers.cs
@@ -6,6 +6,7 @@ using System.Linq;
 // i.e. :
 // inputLineN = 203 204 205 206 207 208 203 204 205 206
 // inputLineM = 203 204 204 205 206 207 205 208 203 206 205 206 204
+// Output : 204 205 206
 
 namespace HackerRank
 {
